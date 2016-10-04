e59c51f9322c49e3c1092bfa1fea46451f00f877
PerunPlayer <prd.6666@abv.bg>
2016-10-04 21:24:46 +0300

    HQC update
    HQC DevelopmentTools Homework added

---
+++ C# High Quality Code/DevelopmentTools/Log4NetDemo/Log4NetDemo/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using log4net;
+using log4net.Config;
+
+namespace Log4NetExample
+{
+    public static class Logger
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Logger));
+
+        public static void Main()
+        {
+            XmlConfigurator.Configure();
+            Log.Info("Info logging");
+            try
+            {
+                throw new Exception("Exception!");
+            }
+            catch (Exception e)
+            {
+                Log.Error("This is my error", e);
+            }
+
+            Console.WriteLine("[any key to exit]");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C# High Quality Code/DevelopmentTools/Log4NetDemo/Log4NetDemo/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿
+namespace MusicPlayer
+{
+	class Tracks
+	{
+              public int Track1 { get; set; }
+              public int Track2 { get; set; }
+              public int Track3 { get; set; }
+              public int Track4 { get; set; }
+              public int Track5 { get; set; }
+              public int Track6 { get; set; }
+              public int Track7 { get; set; }
+              public int Track8 { get; set; }
+              public int Track9 { get; set; }
+              public int Track10 { get; set; }
+              public int Track11 { get; set; }
+              public int Track12 { get; set; }
+              public int Track13 { get; set; }
+              public int Track14 { get; set; }
+              public int Track15 { get; set; }
+              public int Track16 { get; set; }
+              public int Track17 { get; set; }
+              public int Track18 { get; set; }
+              public int Track19 { get; set; }
+              public int Track20 { get; set; }
+    	}
+}
